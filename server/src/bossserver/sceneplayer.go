0747f19caca8c361802e25f860fe0b53202c1b54
xzwwx <844489851@qq.com>
2021-08-11 20:41:03 +0800

    Update sceneplayer.go

--- server/src/bossserver/sceneplayer.go
+++ server/src/bossserver/sceneplayer.go
@@ -16,10 +16,10 @@ type ScenePlayer struct {
 	key  string // login key
 
 	self       *PlayerTask
-	playertask *PlayerTask
+	// playertask *PlayerTask
 	scene      *Scene
 
-	curflag      map[uint32]*ScenePlayer
+	//curflag      map[uint32]*ScenePlayer
 	otherPlayers map[uint64]*ScenePlayer
 	rangeBombs   []*Bomb //current bombs
 	bombs        []*Bomb
@@ -31,7 +31,8 @@ type ScenePlayer struct {
 	isMove    bool
 	pos       Vector2
 	nextpos   Vector2
-	speed     float64
+	speed     uint32
+	power     uint32
 	direction uint32
 	lifeState uint32 // 0: dead   1: alive 	2: jelly
 	bombNum   uint32 //
@@ -51,13 +52,13 @@ func NewScenePlayer(player *PlayerTask, scene *Scene) *ScenePlayer {
 	s := &ScenePlayer{
 		id:         player.id,
 		scene:      scene,
-		playertask: player,
-		curflag:    make(map[uint32]*ScenePlayer),
-		senddie:    false,
+		self:         player,
+		otherPlayers: make(map[uint64]*ScenePlayer),
+		senddie:      false,
 		pos: Vector2{
-			x: r.Uint32(),
-			y: r.Uint32(),
-		},
+			x: r.Float64(),
+			y: r.Float64(),
+		}
 	}
 	return s
 }
@@ -96,7 +97,7 @@ func (this *ScenePlayer) sendSceneMsg(scene *Scene) {
 	// players move msg
 	for _, playermove := range this.otherPlayers {
 		if playermove.isMove {
-			move := &usercmd.BallMove{
+			move := &usercmd.MsgPlayerMove{
 				Id: playermove.id,
 				X:  int32(playermove.pos.x),
 				Y:  int32(playermove.pos.y),
@@ -142,25 +143,36 @@ func (this *ScenePlayer) TimeAction(room *Room, timenow time.Time) bool {
 }
 
 //Lay bomb
-func (this *ScenePlayer) LayBomb(room *Room) {
+func (this *ScenePlayer) LayBomb(room *Room, x, y int32) {
 
 	var (
 		isLayBomb = false
 		scene     = &room.Scene
 	)
 	if this.bombLeft > 0 {
-		cell := scene.GetCellState(this.pos.x, this.pos.y)
+		// cell := scene.GetCellState(uint32(this.pos.x), uint32(this.pos.y))
+		cell := scene.GetCellState(uint32(x), uint32(y))
 		if cell == 0 { // ke fang zha dan
 			timenow := time.Now().Unix()
 			bomb := &Bomb{
-				pos:      this.pos,
+				pos: VectorInt{
+					// X: int32(this.pos.x),
+					// Y: int32(this.pos.y),
+					X: x,
+					Y: y,
+				},
 				player:   this,
 				layTime:  timenow,
 				isdelete: false,
 			}
-			this.rangeBombs = append(this.rangeBombs, bomb)
 			this.bombLeft--
-			scene.rangeBalls = append(scene.rangeBalls, bomb)
+
+			// this.rangeBombs = append(this.rangeBombs, bomb)
+			// scene.rangeBalls = append(scene.rangeBalls, bomb)
+
+			// 另一种形式管理炸弹
+			room.bombmgr.Add(bomb)
+
 			isLayBomb = true
 		}
 
@@ -185,6 +197,12 @@ func (this *ScenePlayer) SendCmd(cmd usercmd.MsgTypeCmd, msg common.Message) boo
 	this.AsyncSend(data, 0)
 	return true
 }
+
+// 发送状态
+func (this *ScenePlayer) SendState(room *Room) {
+	room.BroadcastMsg(usercmd.MsgTypeCmd_PlayerState, PlayerStateCmd(this.id, int32(this.lifeState)))
+}
+
 
 //////////ScenePlayer manager//////////
 type ScenePlayerMgr struct {
@@ -228,7 +246,6 @@ func (this *ScenePlayerMgr) Removes(splayers map[uint64]*ScenePlayer) {
 	this.mutex.Unlock()
 }
 
-
 ////////////
 func (this *ScenePlayer) Move(scene *Scene, speed, direction float64) {
 
@@ -237,15 +254,37 @@ func (this *ScenePlayer) Move(scene *Scene, speed, direction float64) {
 	this.scene.BorderCheck(&this.nextpos) // 保证计算得到的下一位置不超出地图范围
 
 	// TODO 判断移动路径上是否有障碍物
-
-	this.MoveVec(scene, speed, direction)
+	cellstate := scene.GetCellState(uint32(this.nextpos.x), uint32(this.nextpos.y))
+	if cellstate == 0 {
+		this.MoveVec(scene, speed, direction)
+	} else if cellstate == 3 {
+		// 吃道具
+		this.MoveVec(scene, speed, direction)
+		obj := scene.GetObjType(int32(this.nextpos.x), int32(this.nextpos.y))
+		switch obj { // 1: 加速 speed   2：威力 power  3：数量  Bombnum
+		case 1:
+			this.speed++
+			atomic.StoreInt32(&scene.gameMap.gamemap[int32(this.nextpos.x)][int32(this.nextpos.y)], 0)
+		case 2:
+			this.power++
+			atomic.StoreInt32(&scene.gameMap.gamemap[int32(this.nextpos.x)][int32(this.nextpos.y)], 0)
+		case 3:
+			this.bombNum++
+			atomic.StoreInt32(&scene.gameMap.gamemap[int32(this.nextpos.x)][int32(this.nextpos.y)], 0)
+		}
+	} else {
+		// 位置不改变
+	}
 }
+
 func (this *ScenePlayer) MoveVec(scene *Scene, speed, direction float64) {
-	//this.X =
+	this.pos = this.nextpos
 }
 
 // 计算下一个位置
 func (this *ScenePlayer) CaculateNext(direction float64) {
-	this.nextpos.x = this.pos.x + math.Sin(direction*math.Pi/180)*this.speed
-	this.nextpos.y = this.pos.y + math.Cos(direction*math.Pi/180)*this.speed
+	this.nextpos.x = this.pos.x + float64(this.speed)*(math.Cos(direction*math.Pi/2))*0.04
+	this.nextpos.y = this.pos.y + float64(this.speed)*(math.Sin(direction*math.Pi/2))*0.04
+
 }
+
